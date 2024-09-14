07c47b56e5aa78154ad7c794b0454a2dfb419c3d
Roman Micuda <roman.micuda@outlook.com>
2024-09-15 00:14:54 +0200

    feat(button): create button for add recipe to favorite

---
+++ src/app/components/AddRecipeFavorite.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AddRecipeFavorite() {
+  return (
+    <div>
+      <button>Add to Favorite</button>
+    </div>
+  );
+}
+
+export default AddRecipeFavorite;
--- src/app/components/RecipeCardList.tsx
+++ src/app/components/RecipeCardList.tsx
@@ -1,12 +1,16 @@
 import { RecipeCardListProps } from "@/types";
 import React from "react";
 import RecipeCard from "./RecipeCard";
+import AddRecipeFavorite from "./AddRecipeFavorite";
 
 const RecipeCardList: React.FC<RecipeCardListProps> = ({ recipes }) => {
   return (
     <div>
-      {recipes.map((recipe) => (
-        <RecipeCard key={recipe.id} recipe={recipe} />
+      {recipes.map((recipe, index) => (
+        <div key={index}>
+          <RecipeCard key={recipe.id} recipe={recipe} />
+          <AddRecipeFavorite />
+        </div>
       ))}
     </div>
   );
